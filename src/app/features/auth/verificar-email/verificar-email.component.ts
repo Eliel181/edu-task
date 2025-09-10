46bfcc0595416111d2bb70d68f02601a953fdce8
Matias Jaime Casimiro <44516457@fi.unju.edu.ar>
2025-09-10 18:01:36 -0300

    Cambios en routes, component auth.service register y creacion del componente verificar-email

---
+++ src/app/features/auth/verificar-email/verificar-email.component.ts
@@ -0,0 +1,28 @@
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-verificar-email',
+  imports: [],
+  templateUrl: './verificar-email.component.html',
+  styleUrl: './verificar-email.component.css'
+})
+export class VerificarEmailComponent {
+  private router: Router = inject(Router);
+  email: string = '';
+  
+  constructor() {
+    // Obtener el email desde sessionStorage
+    this.email = sessionStorage.getItem('pendingVerificationEmail') || '';
+    // si no hay email redirigimos
+    // if (!this.email) {
+    //   this.router.navigate(['/login']);
+    // }
+  }
+
+  volverLogin() {
+    // Limpiamos el sessionStorage
+    sessionStorage.removeItem('pendingVerificationEmail');
+    this.router.navigate(['/login']);
+  }
+}
