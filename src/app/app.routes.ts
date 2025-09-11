382df9668dd64d40448c1d9706f769ebf9735fed
Matias Jaime Casimiro <44516457@fi.unju.edu.ar>
2025-09-11 11:47:33 -0300

    Agregando cambios en el login, register y route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,15 +2,18 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
-    path:'login', loadComponent:()=>import('./features/auth/login/login.component').then(m => m.LoginComponent)
+    path: 'login', loadComponent: () => import('./features/auth/login/login.component').then(m => m.LoginComponent)
   },
   {
-    path:'register', loadComponent:() => import('./features/auth/register/register.component').then(m => m.RegisterComponent)
+    path: 'register', loadComponent: () => import('./features/auth/register/register.component').then(m => m.RegisterComponent)
   },
   {
-    path:'verificar-email', loadComponent:() => import('./features/auth/verificar-email/verificar-email.component').then(m => m.VerificarEmailComponent)
+    path: 'verificar-email', loadComponent: () => import('./features/auth/verificar-email/verificar-email.component').then(m => m.VerificarEmailComponent)
   },
   {
-    path:'gestion-usuarios', loadComponent:() => import('./features/admin/gestion-usuarios/gestion-usuarios.component').then(m => m.GestionUsuariosComponent)
+    path: 'reset-password', loadComponent: () => import('./features/auth/forgot-password/forgot-password.component').then(m => m.ForgotPasswordComponent)
+  },
+  {
+    path: 'gestion-usuarios', loadComponent: () => import('./features/admin/gestion-usuarios/gestion-usuarios.component').then(m => m.GestionUsuariosComponent)
   },
 ];
