ed2401d0122fe955a12a0e464ae51d452c468034
Eliel181 <baleroeliel@gmail.com>
2025-09-17 00:42:38 -0300

    Configuracion Base para el Modulo de Escuelas

---
+++ src/app/core/interfaces/escuela.model.ts
@@ -0,0 +1,18 @@
+export type NivelEducativo = 'Primario' | 'Secundario' | 'Terciario' | 'Universitario';
+
+export interface Escuela {
+  nombreCompleto:string;
+  nombreCorto:string;
+  cue:string;
+  direccion:string;
+  telefono:number;
+  email:number;
+  paginaWeb:string;
+  director:string; //uid de un usuario con el rol de "Director"
+  matriculaTotal:string;
+  nivel:NivelEducativo;
+  cantAlumnos:number;
+  cantDocentes:number;
+  cantAdministrativos:number;
+  cantEmpleados:number;
+}
---
+++ src/app/features/schools/school-management/school-management.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-school-management',
+  imports: [],
+  templateUrl: './school-management.component.html',
+  styleUrl: './school-management.component.css'
+})
+export class SchoolManagementComponent {
+
+}
