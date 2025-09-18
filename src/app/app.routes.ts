320c7aadc7c3dd8b639cd594eb32444f68ebfda3
Eliel181 <baleroeliel@gmail.com>
2025-09-18 03:37:56 -0300

    Resolucion de conflictos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -49,9 +49,9 @@ export const routes: Routes = [
   },
 
   // Ruta para las tareas de un empleado
-  {
-    //path: 'mis-tareas', loadComponent: () => import('./features/employee/mis-tareas/mis-tareas.component').then(m => m.MisTareasComponent), canActivate: [authGuard]
-  },
+  // {
+  //   path: 'mis-tareas', loadComponent: () => import('./features/employee/mis-tareas/mis-tareas.component').then(m => m.MisTareasComponent), canActivate: [authGuard]
+  // },
 
 
   {
