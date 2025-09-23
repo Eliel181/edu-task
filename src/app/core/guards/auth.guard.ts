11945edd2600355bbde6384fd26cbe94225b5728
Matias Jaime Casimiro <44516457@fi.unju.edu.ar>
2025-09-23 03:38:42 -0300

    Cambios zarpados (Sidebar, Routes, Guard Auth) y quite componentes navbar

--- src/app/core/guards/auth.guard.ts
+++ src/app/core/guards/auth.guard.ts
@@ -2,24 +2,37 @@ import { CanActivateFn, Router } from '@angular/router';
 import { AuthService } from '../services/auth.service';
 import { inject } from '@angular/core';
 import { toObservable } from '@angular/core/rxjs-interop';
-import { map } from 'rxjs';
+import { filter, map, take } from 'rxjs';
+import { Usuario } from '../interfaces/usuario.model';
 
-export const authGuard: CanActivateFn = (route, state) => {
+export const authGuard: CanActivateFn = async (route, state) => {
   const authService: AuthService = inject(AuthService);
   const router: Router = inject(Router);
 
-  return toObservable(authService.currentUser).pipe(
-    map(user => {
-      // Si hay un user pero no se autentico
-      if (user === undefined) {
-        return false;
-      }
-      if (user) {
-        return true;
-      } else {
-        router.navigate(['/login']);
-        return false;
-      }
-    })
-  );
+  // return toObservable(authService.currentUser).pipe(
+  //   // Ignorar valores indefinidos
+  //   filter((user): user is Usuario | null => user !== undefined),
+  //   map(user => {
+  //     if (user) {
+  //       console.log('Usuario autenticado', user);
+        
+  //       return true; // usuario logueado
+  //     } else {
+  //       console.log('Usuario NO autenticado', user);
+  //       router.navigate(['/login']); // no logueado
+  //       return false;
+  //     }
+  //   })
+  // );
+
+  const user = await authService.ready; // espera al primer valor de Firebase
+
+  if (user) {
+    console.log('Usuario autenticado', user);
+    return true;
+  } else {
+    console.log('Usuario NO autenticado', user);
+    router.navigate(['/login']);
+    return false;
+  }
 };
