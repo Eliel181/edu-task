34246bbdef36a17b51c5fcf0466c8680469d7e6e
Matias Jaime Casimiro <44516457@fi.unju.edu.ar>
2025-09-23 21:14:59 -0300

    Cree un Spinner de carga

--- src/app/core/guards/auth.guard.ts
+++ src/app/core/guards/auth.guard.ts
@@ -1,31 +1,12 @@
 import { CanActivateFn, Router } from '@angular/router';
 import { AuthService } from '../services/auth.service';
 import { inject } from '@angular/core';
-import { toObservable } from '@angular/core/rxjs-interop';
-import { filter, map, take } from 'rxjs';
-import { Usuario } from '../interfaces/usuario.model';
 
 export const authGuard: CanActivateFn = async (route, state) => {
   const authService: AuthService = inject(AuthService);
   const router: Router = inject(Router);
 
-  // return toObservable(authService.currentUser).pipe(
-  //   // Ignorar valores indefinidos
-  //   filter((user): user is Usuario | null => user !== undefined),
-  //   map(user => {
-  //     if (user) {
-  //       console.log('Usuario autenticado', user);
-        
-  //       return true; // usuario logueado
-  //     } else {
-  //       console.log('Usuario NO autenticado', user);
-  //       router.navigate(['/login']); // no logueado
-  //       return false;
-  //     }
-  //   })
-  // );
-
-  const user = await authService.ready; // espera al primer valor de Firebase
+  const user = await authService.ready; 
 
   if (user) {
     console.log('Usuario autenticado', user);
